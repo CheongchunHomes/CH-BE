e7aa4510d86120befd261b1fe2cef723e4169c61
kimdoha-kikilove <kikiwadoha@gmail.com>
2026-04-30 19:04:11 +0900

    [recommend] url 수정

--- src/main/java/com/chcorp/homes/recommend_test/controller/RecController.java
+++ src/main/java/com/chcorp/homes/recommend_test/controller/RecController.java
@@ -11,8 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/recommend")
-@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/recommend")
 @RequiredArgsConstructor
 public class RecController {
 
