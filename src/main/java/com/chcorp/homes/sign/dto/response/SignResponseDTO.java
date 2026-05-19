9049f9acb17ee68837f5ae6592d3fb06c6393f5e
GarlGaru <juon304@gmail.com>
2026-05-19 21:14:36 +0900

    [계약] fix : 계약서 목록 표시 추가

--- src/main/java/com/chcorp/homes/sign/dto/response/SignResponseDTO.java
+++ src/main/java/com/chcorp/homes/sign/dto/response/SignResponseDTO.java
@@ -8,8 +8,12 @@ import java.time.Instant;
 public record SignResponseDTO(
         Long signId,
         Long providerId,
+        String providerNickname,
         Long customerId,
+        String customerNickname,
         Long propertyId,
+        String propertyTitle,
+        String propertyAddress,
         SignStatus status,
         Instant createdAt,
         Instant updatedAt
@@ -18,8 +22,12 @@ public record SignResponseDTO(
         return new SignResponseDTO(
                 signRequest.getId(),
                 signRequest.getProvider().getId(),
+                signRequest.getProvider().getNickname(),
                 signRequest.getCustomer().getId(),
+                signRequest.getCustomer().getNickname(),
                 signRequest.getPropertyId().getId(),
+                signRequest.getPropertyId().getTitle(),
+                signRequest.getPropertyId().getAddress(),
                 signRequest.getStatus(),
                 signRequest.getCreatedAt(),
                 signRequest.getUpdatedAt()
